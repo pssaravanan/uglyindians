fd3005be23d55905498d0f1e9f55a75624fd2524
Rajik <krajeswaribtech@gmail.com>
2014-09-27 00:55:58 +0530

    [Sudhakar/Raji] Here's the splash screen

---
+++ app/src/main/java/tw/uglyindian/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package tw.uglyindian;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
