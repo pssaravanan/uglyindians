c0c6238894fd4b9b8c8ab7672d10eb740a1af24e
Somasundaram Mahesh <msomu@ymail.com>
2014-09-27 05:05:55 +0530

    Send Mail id on Report

--- app/src/main/java/tw/uglyindian/ReportActivity.java
+++ app/src/main/java/tw/uglyindian/ReportActivity.java
@@ -1,7 +1,9 @@
 package tw.uglyindian;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -34,12 +36,16 @@ public class ReportActivity extends Activity {
     ImageView imgPost;
     final static int cameraData=0;
     Bitmap bmp;
-    String encoded;
+    String encoded,eMail;
+    SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+
+
         etDescription = (EditText) findViewById(R.id.etDescriptions);
         btnReport = (Button) findViewById(R.id.btnReport);
         imgPost = (ImageView) findViewById(R.id.imgPost);
@@ -56,10 +62,18 @@ public class ReportActivity extends Activity {
             @Override
             public void onClick(View view) {
                 // click to report
+                getMail();
+                if(bmp!=null)
                 decodeBmp();
                 new LongOperation().execute("");
             }
         });
+    }
+
+    private void getMail() {
+        String the_ugly_indian_prefs = "THE_UGLY_INDIAN_PREFS";
+        sharedpreferences = getSharedPreferences(the_ugly_indian_prefs, Context.MODE_PRIVATE);
+        eMail = sharedpreferences.getString("email","");
     }
 
     private void decodeBmp() {
@@ -98,6 +112,7 @@ public class ReportActivity extends Activity {
                 nameSpotDetails.add(new BasicNameValuePair("event_name", "clean"));
                 nameSpotDetails.add(new BasicNameValuePair("event_description",desc ));
                 nameSpotDetails.add(new BasicNameValuePair("image", encoded));
+                nameSpotDetails.add(new BasicNameValuePair("initiator", eMail));
                 post.setEntity(new UrlEncodedFormEntity(nameSpotDetails));
 
                 // Execute HTTP Post Request
@@ -117,7 +132,8 @@ public class ReportActivity extends Activity {
         protected void onPostExecute(String result) {
             // might want to change "executed" for the returned string passed
             // into onPostExecute() but that is upto you
-            Toast.makeText(ReportActivity.this,"Posted",Toast.LENGTH_SHORT).show();
+            Toast.makeText(ReportActivity.this,"Posted Successfully",Toast.LENGTH_SHORT).show();
+            finish();
         }
 
         @Override
